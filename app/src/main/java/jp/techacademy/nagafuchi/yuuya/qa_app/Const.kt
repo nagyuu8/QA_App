dc906c0204b09551201bdb570830f7443d92f45b
nagyuu8 <7yuntxcdl@gmail.com>
2020-10-10 22:10:25 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/nagafuchi/yuuya/qa_app/Const.kt
@@ -0,0 +1,6 @@
+package jp.techacademy.nagafuchi.yuuya.qa_app
+
+const val UsersPATH = "users"//Firebaseにユーザーの表示名を保存するパス
+const val ContentsPATH = "contents" //Firebaseに質問を保存するパス
+const val AnswersPATH = "answers" //Firebaseに解答を保存するパス
+const val NameKEY = "name" //Preferenceに表示名を保存するときのキー
