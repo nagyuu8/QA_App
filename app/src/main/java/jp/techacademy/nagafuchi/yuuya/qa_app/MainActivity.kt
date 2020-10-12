2d55b41e7605f53036fcd50c0ddc94140a5a8e81
nagyuu8 <7yuntxcdl@gmail.com>
2020-10-12 21:30:30 +0900

    Chp7終了

--- app/src/main/java/jp/techacademy/nagafuchi/yuuya/qa_app/MainActivity.kt
+++ app/src/main/java/jp/techacademy/nagafuchi/yuuya/qa_app/MainActivity.kt
@@ -3,7 +3,11 @@ package jp.techacademy.nagafuchi.yuuya.qa_app
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.NavigationView
 import android.support.design.widget.Snackbar
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseUser
 import android.support.v7.widget.Toolbar
@@ -14,25 +18,58 @@ import com.google.firebase.auth.FirebaseAuth
 
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
+
+
     private lateinit var mToolbar: Toolbar
+    private var mGenre = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mToolbar = findViewById(R.id.toolbar)
         setSupportActionBar(mToolbar)
+
         val fab = findViewById<FloatingActionButton>(R.id.fab)
-        fab.setOnClickListener{ v ->
+        fab.setOnClickListener{ view ->
+            //ジャンルをジャンルを選択していない場合(mGenre == 0)はエラーを表示するだけにする
+            if(mGenre == 0){
+                Snackbar.make(view,"ジャンルを選択してください。",Snackbar.LENGTH_LONG).show()
+
+            }else{
+
+            }
             //ログイン済みのユーザーを取得する
             val user = FirebaseAuth.getInstance().currentUser
-
 
             //ログインしていなければログイン画面に遷移させる。
             if(user == null){
                 val intent = Intent(applicationContext,LoginActivity::class.java)
                 startActivity(intent)
+            } else{
+                //ジャンルを渡して質問作成画面を起動する
+                val intent = Intent(applicationContext,QuestionSendActivity::class.java)
+                intent.putExtra("genre",mGenre)
+                startActivity(intent)
             }
+        }
+        //ナビゲーションドロワーの設定
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val toggle = ActionBarDrawerToggle(this,drawer,mToolbar,R.string.app_name,R.string.app_name)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+
+        //趣味を規定の選択とする
+        if(mGenre == 0){
+            onNavigationItemSelected(navigationView.menu.getItem(0))
         }
     }
 
@@ -46,9 +83,37 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        when (item.itemId) {
+            R.id.action_settings -> {
+                val intent = Intent(applicationContext,SettingActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
         }
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        when(id){
+            R.id.nav_hobby ->{
+                mToolbar.title ="趣味"
+                mGenre =1
+            }
+            R.id.nav_life ->{
+                mToolbar.title="生活"
+                mGenre = 2
+            }
+            R.id.nav_health ->{
+                mToolbar.title="健康"
+                mGenre = 3
+            }
+            R.id.nav_computer ->{
+                mToolbar.title="コンピューター"
+                mGenre = 4
+            }
+        }
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        drawer.closeDrawer(GravityCompat.START)
+        return true
     }
 }
